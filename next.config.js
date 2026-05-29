d0279fdd9307b599c5ad48d672468280bd40d523
Gramms <hardekhalu@gmail.com>
2026-05-29 23:49:29 +0000

    fix: use relative image paths with basePath, add unoptimized for static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/Lamborghini-Miura-showcase',
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
